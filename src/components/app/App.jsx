4f6b2fec285e572f99dd4496fb8d373eeaaeea2e
Victor <vix52243038@bk.ru>
2022-05-02 20:09:41 +0300

    feat:Сделал шапку сайта

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import AppHeader from '../app-header/app-header.jsx'
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+
+    </div>
+  );
+}
+
+export default App;
