a77eda4ac8e59c40323fff4a3fe42a4bda1a707d
Victor Stepanov <vix522430@bk.ru>
2022-10-07 17:26:22 +0300

    test

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -3,7 +3,7 @@ import {
     useDispatch as dispatchHook,
     useSelector as selectorHook
   } from 'react-redux';
-  import { AppDispatch, RootState, AppThunk } from '../services/types/index';
+  import { AppDispatch, RootState} from '../services/types/index';
   
   export const useDispatch = () => dispatchHook<AppDispatch>();
   export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
