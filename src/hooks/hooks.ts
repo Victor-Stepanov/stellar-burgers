178a4493b44572d13c7b5603bc4b0beb2021393f
Victor Stepanov <vix522430@bk.ru>
2022-10-13 17:49:22 +0300

    fix

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,5 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
   import { AppDispatch, RootState, AppThunk} from '../services/types/index';
   
-  export const useAppDispatch: () => AppDispatch = useDispatch
+  export const useAppDispatch: () => AppDispatch| AppThunk = useDispatch
   export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
